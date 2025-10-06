1dcb712e54dc453792db16afaa7e5391e7b41cac
Fran-educaand <fgongar2711@g.educaand.es>
2025-10-06 20:27:57 +0200

    Nuevos cambios

---
+++ Ud1/Variable y Tipos de datos/Ejercicio1.py
@@ -0,0 +1,25 @@
+
+
+edad  = 19
+print(type(edad))
+
+nombre = "Fran"
+print ( type(nombre))
+
+es_estudiante = True
+print ( type(es_estudiante))
+
+pi = 3.142
+print ( type(pi))
+
+temperatura = 25
+del(temperatura)
+print(temperatura)
+
+# NameError: name 'temperatura' is not defined
+
+x = 1
+y = 2
+z= 3
+del(y)
+print(y)
