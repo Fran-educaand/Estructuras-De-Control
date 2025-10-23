3c21963539da4068af77f446440058b81edfe5a1
Fran-educaand <fgongar2711@g.educaand.es>
2025-10-23 18:59:03 +0200

    nuevas cosas

--- Ud1/Cuaderno 6/Exclamaciones.py
+++ Ud1/Cuaderno 6/Exclamaciones.py
@@ -5,6 +5,7 @@
 class MiExcepcionPersonalizada(Exception):
     def __init__(self, mensaje):
         super().__init__(mensaje)
+    pass
 
 
 
@@ -59,14 +60,34 @@ except:   print("error")
 # lanzará una excepción personalizada (por ejemplo: `class CadenasDemasiadoCortasError(Exception):` que no hará nada `pass`). Si la cadena tiene más de 3 caracteres, la imprimirá,
 #  Recorrerá todas las cadenas, debe continuar incluso si se lanza una excepción para algunas cadenas.
 
+try : 
 
+    lista_cadena = input("Escribe una lista de cadenas: ").split(" ")
 
+    for i in lista_cadena:
+        if len(i) < 3:
+           try:
+             raise MiExcepcionPersonalizada("¡Algo salió mal!")
+           except MiExcepcionPersonalizada as e:
+                print(e)
+        else:print(i)
+      
+
+except :  print ("Error")
 
 
 
 ##❗**Ejercicio 27:** Implementa una función recursiva que calcule el factorial de un número. 
 # Si el número es negativo, lanza una excepción (como `class FactorialNegativoError(Exception):`) indicando que no se puede calcular el factorial de números negativos.
+class FactorialNegativoError(Exception):
+    def __init__(self, mensaje):
+        super().__init__(mensaje)
 
+    pass
+def funRecursiva(n):
+    if(n < 0):
+        return FactorialNegativoError("No puede ser negativo")
+    else: pass
 
 
 
@@ -74,6 +95,45 @@ except:   print("error")
 ##- **Ejercicio 28:** Crea una función que reciba una lista de números y devuelva el mayor número que aparece en la lista. Si la lista está vacía, lanzará una excepción.
 
 
+def mayor(lista_num):
+
+    if len(lista_num)<1:
+        raise  MiExcepcionPersonalizada("No hay nada")
+    else:
+        num = 0
+        for i in lista_num:
+            if (i > num):
+                num = i
+          
+        print (num)   
+
 
 ## ❗**Ejercicio 29:** Crea una clase Producto que tenga un nombre y un precio.
 #  Luego, implementa una clase Carrito que permita agregar productos al carrito, calcular el total, y eliminar productos. Si, al crear un producto, su precio es negativo, lanzará una excepción.
+class Producto:
+    def __init__(self,nombre,precio):
+        self.nombre = nombre
+        if(precio <0):
+            raise MiExcepcionPersonalizada("Precio es 0")
+        else:
+            self.precio = precio
+
+    def getPrecio(self):
+        return self.precio
+    
+class Carrito:
+    def __init__(self):
+        self.listaProducto =[]
+
+    def añadirProducto(self,producto):
+        self.listaProducto.append(producto)
+    
+    def calcPrecio(self):
+        num = 0
+        for producto in self.listaProducto:
+            num += producto.getPrecio()
+        return f"El precio total es : {num}"
+    
+    
+    def quitarProducto(self,producto):
+        self.listaProducto.remove(producto)
